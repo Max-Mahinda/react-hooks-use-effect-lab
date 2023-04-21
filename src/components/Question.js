8e01935fbf69df0f49856b48cd37e4f6c70d9921
Max <maxmwangikenya@gmail.com>
2023-04-21 19:41:54 +0300

    completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,31 +1,32 @@
-import React, { useState } from "react";
+import { useEffect, useState } from 'react';
 
-function Question({ question, onAnswered }) {
+function Question({ onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining(prevTime => prevTime - 1);
+    }, 1000);
 
-  function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
-  }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeRemaining]);
 
-  const { id, prompt, answers, correctIndex } = question;
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]);
 
   return (
-    <>
-      <h1>Question {id}</h1>
-      <h3>{prompt}</h3>
-      {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
-        return (
-          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
-            {answer}
-          </button>
-        );
-      })}
-      <h5>{timeRemaining} seconds remaining</h5>
-    </>
+    <div>
+      <h2>Time Remaining: {timeRemaining}</h2>
+      <p>What is the capital of France?</p>
+      <button onClick={() => onAnswered(true)}>Paris</button>
+      <button onClick={() => onAnswered(false)}>Berlin</button>
+    </div>
   );
 }
 
